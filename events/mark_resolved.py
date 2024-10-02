c2b32a04d74a782371619e1ac92755dc7c3afc08
Dillon Barnes <dillonb07dev@gmail.com>
2024-10-02 10:52:46 +0100

    reimagine resolving

--- events/mark_resolved.py
+++ events/mark_resolved.py
@@ -3,34 +3,18 @@ from typing import Dict, Any
 
 from utils.env import env
 
-import time
-
 
 async def handle_mark_resolved(body: Dict[str, Any], client: AsyncWebClient):
     res = env.airtable.resolve_request(body["message"]["ts"], body["user"]["id"])
     if not res:
         return
 
-    msg = await client.conversations_history(
-        channel=env.slack_request_channel,
-        latest=body["message"]["ts"],
-        limit=1,
-        inclusive=True
+    await client.reactions_add(
+        channel=env.slack_support_channel,
+        name="white_check_mark",
+        timestamp=res["fields"]["identifier"],
     )
 
-    blocks = msg["messages"][0]["blocks"]
-    new_blocks = []
-
-    for i, block in enumerate(blocks):
-        if block["type"] == "rich_text":
-            new_blocks.append(block)
-        if block["type"] == "context":
-            blocks[i]["elements"][0]["text"] = f"Resolved by <@{body['user']['id']}> at {time.strftime('%H:%M:%S %d/%m/%y %Z')}"
-            new_blocks.append(block)
-
-    await client.chat_update(
-        channel=env.slack_request_channel,
-        ts=body["message"]["ts"],
-        text=msg["messages"][0]["text"],
-        blocks=new_blocks
+    await client.chat_delete(
+        channel=env.slack_request_channel, ts=body["message"]["ts"]
     )
